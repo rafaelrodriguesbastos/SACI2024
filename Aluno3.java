67362826d2fdc71b8603759610914ab0f2c9df5d
rafaelrodriguesbastos <rafaelrodriguesbastos@gmail.com>
2024-09-12 19:32:27 -0300

    Add files via upload

--- Aluno3.java
+++ Aluno3.java
@@ -10,5 +10,13 @@ public class Aluno3 {
 		this.media = media; 
 	} 
 	
+	public static void main(String args[]) {
+	  Aluno3 ob1 = new Aluno3("Rafael", 7.8);
+	  Aluno3 ob2 = new Aluno3("Cadu");
+	  
+	  System.out.println("Aluno 1: " + ob1.nome + " média " + ob1.media);
+	  System.out.println("Aluno 2: " + ob2.nome + " média " + ob2.media);
+	
+	}	
 }
 
